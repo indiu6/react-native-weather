8604b52ffdef9d73843734eab047498e37e18fd2
indiu6 <hewas6@gmail.com>
2021-01-12 15:17:05 -0500

    error of getting condition state

--- App.js
+++ App.js
@@ -1,8 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
 // <StatusBar style="auto" />
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Alert } from 'react-native';
 import Loading from './Loading';
+import Weather from './Weather';
+import * as Location from 'expo-location';
+import axios from 'axios';
+
+const WEATHER_API_KEY = 'bfe68bf678b7f62d1f2c3d85887a49e5';
 
 export default function App() {
-  return <Loading />;
+  const [isLoading, setLoading] = useState(true);
+  const [dataSet, setData] = useState({
+    temp: null,
+    condition: null,
+    name: null,
+  });
+
+  useEffect(() => {
+    getLoaction();
+  }, []);
+
+  const getLoaction = async () => {
+    try {
+      // throw Error(); // to test error msg
+      await Location.requestPermissionsAsync();
+      let {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+      // console.log(latitude, longitude);
+
+      getWeather(latitude, longitude);
+    } catch (error) {
+      Alert.alert("Can't find you", 'So sad');
+    }
+  };
+
+  const getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}&units=metric`,
+    );
+    console.log(data);
+
+    let {
+      main: { temp },
+      weather,
+      name,
+    } = data;
+    // console.log(weather[0].main);
+
+    setLoading(false);
+    setData({
+      temp: temp,
+      condition: weather[0].main,
+      name: name,
+    });
+    // console.log(dataSet.temp);
+    // console.log(dataSet.condition);
+  };
+
+  return isLoading ? (
+    <Loading />
+  ) : (
+    <Weather
+      temp={Math.round(dataSet.temp)}
+      condition={dataSet.condition}
+      name={dataSet.name}
+    />
+  );
 }
--- README.md
+++ README.md
@@ -2,8 +2,14 @@
 
 Learning React Native by building a Weather App using Expo https://docs.expo.io/
 
+## Weather API
+
+https://openweathermap.org/api
+
 ## Deprecated
 
 ### Geolocation Use @react-native-community/geolocation instead.
 
 https://github.com/react-native-geolocation/react-native-geolocation
+
+https://docs.expo.io/versions/latest/sdk/location/
